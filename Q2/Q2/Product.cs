cecc504fb0794656599651272437a1d2b5629f64
wasay-sial <233511@students.au.edu.pk>
2025-01-08 15:23:34 +0500

    Add files via upload

---
+++ Q2/Q2/Product.cs
@@ -0,0 +1,45 @@
+﻿namespace Q2
+{
+    public class Product
+    {
+        private string name;
+        private int price;
+        private string category;
+
+
+        public Product(string name, int price, string category)
+        {
+            this.name = name;
+            this.price = price;
+            this.category = category;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setPrice(int price)
+        {
+            this.price = price;
+        }
+
+        public void setCategorty(string cat)
+        {
+            category= cat;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getPrice()
+        {
+           return price;
+        }
+
+        public string getCategory()
+        {
+            return category;
+        }
+    }
+}
